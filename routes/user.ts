730806b26c08ef21fa531adbfa17ad8f1ec9afd6
felix1251 <domhack123@gmail.com>
2022-12-07 00:15:02 +0800

    user types

--- routes/user.ts
+++ routes/user.ts
@@ -21,7 +21,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req:Request, res:Response
 });
 
 //delete a certain user (admin only)
-router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+router.delete("/:id", verifyTokenAndAdmin, async (req:Request, res:Response): Promise<any> => {
       try {
             await User.findByIdAndDelete(req.params.id);
             res.status(200).json("User has been deleted..");
@@ -31,7 +31,7 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 //get single user (only admin)
-router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
+router.get("/:id", verifyTokenAndAdmin, async (req:Request, res: Response): Promise<any> => {
       try {
             const user = await User.findById(req.params.id);
             res.status(200).json(user);
@@ -39,6 +39,17 @@ router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
             res.status(500).json(err);
       }
 });
+
+//get all users (only admin)
+router.get("/", verifyTokenAndAdmin, async (req:Request, res:Response): Promise<any> => {
+      try {
+            const users = await User.find()
+            res.status(200).json(users)
+      } catch (err) {
+            res.status(500).json(err);
+      }
+});
+
 
 
 export default router;
