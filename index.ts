adf01df10fb423ec825f67297d1e951c99146216
felix1251 <domhack123@gmail.com>
2022-12-06 16:34:31 +0800

    setup mongoDB

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
 dotenv.config()
+
+const mongoUrl:string|undefined = process.env.MONGO_URL
+
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoUrl as string)
+      .then(() => console.log("DBconnection Successful"))
+      .catch((err) => {
+            console.log(err);
+      });
 
 app.listen(process.env.PORT || 5000, () => {
       console.log("server is running")
