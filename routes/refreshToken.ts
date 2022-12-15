04d9fce872e9e4b468715d5e60cd37b19d4a92b5
felix1251 <domhack123@gmail.com>
2022-12-15 09:28:44 +0800

    put inside the try catch

--- routes/refreshToken.ts
+++ routes/refreshToken.ts
@@ -7,29 +7,29 @@ router.post('/', async (req:Request, res:Response) => {
   const refreshToken:string|any = req.headers.refreshToken
   if (refreshToken) {
     jwt.verify( refreshToken, process.env.JWT_REFRESH_SEC as string,
-      async (err:any, decodedUser:any) => {
+      async (err:any, decodedUser:any):Promise<any> => {
         if (err) return res.status(406).json({ message: 'Unauthorized',  error: err })
-        let user:any;
         try{
-          user = await User.findOne({ id: decodedUser.id});
+          const user:any = await User.findOne({ id: decodedUser.id});
+          const accessToken = jwt.sign(
+            {
+              id: user._id as string,
+              isAdmin: user.isAdmin as boolean,
+            },
+            process.env.JWT_SEC as string,
+            { expiresIn: "1d" }
+          );
+          const refreshToken = jwt.sign(
+            {
+              id: user._id as string,
+            }, process.env.JWT_REFRESH_SEC as string, 
+            { expiresIn: '7d' }
+          );
+          const { password, ...others } = user._doc;
+          return res.json({...others, accessToken, refreshToken })
         }catch (err2){
           return res.status(406).json({ message: 'Unauthorized', error: err2 })
         }
-        const accessToken = jwt.sign(
-          {
-            id: user._id as string,
-            isAdmin: user.isAdmin as boolean,
-          },
-          process.env.JWT_SEC as string,
-          { expiresIn: "1d" }
-        );
-        const refreshToken = jwt.sign(
-          {
-            id: user._id as string,
-          }, process.env.JWT_REFRESH_SEC as string, 
-          { expiresIn: '7d' }
-        );
-        return res.json({ accessToken, refreshToken })
       },
     )
   } else {
