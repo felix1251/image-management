f4569c5e9dc760dad043e4f9b7a4224b64591810
felix1251 <domhack123@gmail.com>
2022-12-16 23:44:46 +0800

    use async function, should run db before initialiaing routes

--- index.ts
+++ index.ts
@@ -1,26 +1,26 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-import authRoute from "./routes/auth";
-import imageRoute from "./routes/image";
-import refreshTokenRoute from "./routes/refreshToken";
-import userRoute from "./routes/user";
+import authRoute from "./routes/auth"
+import imageRoute from "./routes/image"
+import refreshTokenRoute from "./routes/refreshToken"
+import userRoute from "./routes/user"
 import cors from "cors"
-const app = express()
 dotenv.config()
 
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URL as string)
-      .then(() => console.log("DBconnection Successful"))
-      .catch((err) => console.log(err));
+const run:Function = async ():Promise<void> => {
+      mongoose.set('strictQuery', true);
+      const dbConnected = await mongoose.connect(process.env.MONGO_URL as string)
+      if(dbConnected) console.log("DB is connected")
 
-app.use(cors());
-app.use(express.json());
-app.use("/auth", authRoute);
-app.use("/users", userRoute);
-app.use("/images", imageRoute);
-app.use("/refreshToken", refreshTokenRoute);
+      const app = express()
+      app.use(cors());
+      app.use(express.json());
+      app.use("/auth", authRoute);
+      app.use("/users", userRoute);
+      app.use("/images", imageRoute);
+      app.use("/refreshToken", refreshTokenRoute);
+      app.listen(process.env.PORT || 5000, () => {console.log("server is running")})
+}
 
-app.listen(process.env.PORT || 5000, () => {
-      console.log("server is running")
-})
+run()
